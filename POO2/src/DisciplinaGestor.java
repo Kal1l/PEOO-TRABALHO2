c49f226082829c635d7ce75b7ae742c26f7959c3
Kalil <106711007+Kal1l@users.noreply.github.com>
2022-06-26 09:38:30 -0300

    Delete POO2 directory

--- POO2/src/DisciplinaGestor.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class DisciplinaGestor {
-    
-    public void novoArquivo(File dir,String disciplina){
-        File d = new File(dir,disciplina+".txt");
-        try{d.createNewFile();}catch(IOException e){}
-    }
-
-}
