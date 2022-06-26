31728169ac94ea96cff37ba51fe565ccbbad1d7f
Kalil <106711007+Kal1l@users.noreply.github.com>
2022-06-26 09:38:43 -0300

    Delete PEOO-TRABALHO2-main/POO2 directory

--- PEOO-TRABALHO2-main/POO2/src/exception/GabaritoInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class GabaritoInvalidoException extends Exception{
-    public String toString() {
-        return "Gabarito preenchido errado, insira novamente";
-    }
-}
